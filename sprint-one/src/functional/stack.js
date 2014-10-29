d1973ace54ba97ec05792b68a69b9953ca4f96f3
trizko <trizko>
2014-10-29 14:54:31 -0700

    Completes functional stack module

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[length] = value;
+    length++;
   };
 
   someInstance.pop = function(){
+    if(length > 0) {
+      length--;
+    }
+    var result = storage[length];
+    delete storage[length];
+    return result;
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
