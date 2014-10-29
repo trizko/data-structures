d9c1c2e23c5c7e1e5b599262344c4009016e2827
trizko <trizko>
2014-10-29 15:16:54 -0700

    Completes functional queue module

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,30 @@
 var makeQueue = function(){
   var someInstance = {};
 
+  var length = 0;
+  var removed = 0;
+
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+      storage[length] = value;
+      length++;
   };
 
   someInstance.dequeue = function(){
+    if(length > removed){
+      var result = storage[removed];
+      delete storage[removed];
+      removed++;
+    }
+    return result;
   };
 
   someInstance.size = function(){
+    return length - removed;
   };
 
   return someInstance;
