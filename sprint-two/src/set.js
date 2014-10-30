7a43b198b7531f6acc17e8e5733014c0a5c701ce
trizko <trizko>
2014-10-30 16:37:03 -0700

    Completes set module

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage[item]){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * setPrototype.add has an O(1) time complexity
+ * setPrototype.contains has an O(1) time complexity
+ * setPrototype.remove has an O(1) time complexity
  */
