13d47afe434ede00eae6de28833ad04a91392c35
trizko <trizko>
2014-10-30 10:46:20 -0700

    Completes prototypal queue module

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.length = 0;
+  newQueue.removed = 0;
+  newQueue.storage = {};
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.length > this.removed) {
+    var result = this.storage[this.removed];
+    delete this.storage[this.removed];
+    this.removed++;
+  }
+  return result;
+};
+
+queueMethods.size = function() {
+  return this.length - this.removed;
+};
+
 
 
