ec2acbe7fcfa8d59502fa474642c41511fc45220
trizko <trizko>
2014-10-30 10:43:31 -0700

    Completes prototypal stack module

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.length = 0;
+  newStack.storage = {};
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
 
+stackMethods.pop = function(){
+  if(this.length > 0){
+    this.length--;
+    var result = this.storage[this.length];
+    delete this.storage[this.length];
+  }
+
+  return result;
+};
+
+stackMethods.size = function(){
+  return this.length;
+};
