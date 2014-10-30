f733f095c3a1789251c5cfd51a91c485d572d3e0
trizko <trizko>
2014-10-30 11:12:06 -0700

    Completes pseudoclassical queue module

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.removed = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.length > this.removed){
+    var result = this.storage[this.removed];
+    delete this.storage[this.removed];
+    this.removed++;
+  }
+
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return this.length - this.removed;
+};
 
