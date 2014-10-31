bdbed38afdd451a0b1133750f623fa2cae1de3af
trizko <trizko>
2014-10-30 21:34:11 -0700

    Completes all tests other than collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,25 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(i)){
+    i++;
+  }
+  this._storage.set(i,v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, key, storage){
+    if(i === key){
+      storage[i] = null;
+    }
+  });
 };
 
 
