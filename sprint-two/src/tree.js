bccf8eb200d2c04f9ed70f6bc832bf0db5ee0086
trizko <trizko>
2014-10-31 16:01:04 -0700

    Completes tree module

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,35 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var children = this.children;
+  var isFound = false;
 
+  var inner = function(treeArray){
+    for(var i = 0; i < treeArray.length; i++){
+      if(treeArray[i].value === target){
+        return isFound = true;
+      }
+      if(treeArray[i].children.length > 0){
+        inner(treeArray[i].children);
+      }
+    }
+  };
+
+  inner(children);
+  return isFound;
 };
 
 
